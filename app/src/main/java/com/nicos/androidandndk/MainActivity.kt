e980f430636e9974115c66963bdba1ede0782151
Nicos Nicolaou <nicosnicos16@gmail.com>
2023-12-28 21:38:37 +0200

    minor changes

--- app/src/main/java/com/nicos/androidandndk/MainActivity.kt
+++ app/src/main/java/com/nicos/androidandndk/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import com.nicos.androidandndk.ui.theme.AndroidAndNDKTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +32,7 @@ class MainActivity : ComponentActivity() {
     }
 
     /**
-     * Call method from C file
+     * Initialize method from C file
      * */
     private external fun message(): String
 
@@ -38,19 +45,31 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting(message())
+                    Greeting()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Box(modifier = modifier, contentAlignment = Alignment.Center) {
-        Text(
-            text = name,
-            modifier = modifier
-        )
+    @Composable
+    fun Greeting(modifier: Modifier = Modifier) {
+        var textMessage by rememberSaveable { mutableStateOf("") }
+        Box(modifier = modifier, contentAlignment = Alignment.Center) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(
+                    text = textMessage,
+                    modifier = modifier,
+                    textAlign = TextAlign.Center
+                )
+                Button(onClick = {
+                    textMessage = message()
+                }) {
+                    Text(
+                        text = "Click Here",
+                        modifier = modifier
+                    )
+                }
+            }
+        }
     }
 }
